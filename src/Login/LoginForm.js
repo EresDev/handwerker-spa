538a0b4c66d1efd9b39bc1f8a5b461457d63f0c1
EresDev <hi@eresdev.com>
2020-06-12 02:15:45 +0500

    LoginForm added

--- src/Login/LoginForm.js
+++ src/Login/LoginForm.js
@@ -1,31 +1,59 @@
 import React from 'react';
 
 export default class LoginForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            'email': '',
+            'password': '',
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({[event.target.id]: event.target.value});
+    }
+
+    handleSubmit(event) {
+        console.log(this.state);
+        event.preventDefault();
+    }
+
     render() {
         return (
-            <form name="login_form" method="post" action="/login_check">
+            <form onSubmit={this.handleSubmit}>
                 <div id="login_form">
-                    <div className="field"><label htmlFor="login_form_email"
-                                                  className="required">Email</label><input type="email"
-                                                                                           id="login_form_email"
-                                                                                           name="login_form[email]"
-                                                                                           required="required"/>
+                    <div className="field">
+                        <label htmlFor="email"
+                               className="required">Email</label>
+                        <input type="email"
+                               id="email"
+                               required="required"
+                               value={this.state.email}
+                               onChange={this.handleChange}
+                        />
                     </div>
-                    <div className="field"><label htmlFor="login_form_password"
-                                                  className="required">Password</label><input
-                        type="password" id="login_form_password" name="login_form[password]"
-                        required="required"/></div>
+                    <div className="field">
+                        <label htmlFor="login_form_password"
+                               className="required">Password</label>
+                        <input type="password"
+                               id="password"
+                               value={this.state.password}
+                               onChange={this.handleChange}
+                               required="required"
+                               minLength={6}
+                               maxLength={4096}
+                        />
+                    </div>
                     <div>
                         <ul className="actions">
                             <li>
-                                <button type="submit" id="login_form_Login" name="login_form[Login]"
-                                        className="alt">Login
-                                </button>
+                                <button type="submit" className="alt">Login</button>
                             </li>
                         </ul>
                     </div>
-                    <input type="hidden" id="login_form__token" name="login_form[_token]"
-                           defaultValue="6h42HovTAyJ3H5IZekxcC3oGouy4haDawQ9EcTiTn_g"/></div>
+                </div>
             </form>
         );
     }
