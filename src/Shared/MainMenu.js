5acad2c1844f08b748c2c53623957c94b8a019b8
EresDev <hi@eresdev.com>
2020-07-08 03:24:37 +0500

    Added logout button

--- src/Shared/MainMenu.js
+++ src/Shared/MainMenu.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router,
-  Link
-} from "react-router-dom";
-import i18n from "i18next";
+import {Link} from 'react-router-dom';
+import i18n from 'i18next';
 import {initReactI18next} from 'react-i18next';
+import Security from '../Util/Security';
 
-export default class MainMenu extends React.Component{
+export default class MainMenu extends React.Component {
     constructor(props) {
         super(props);
         this.switchLocale = this.switchLocale.bind(this);
+        this.logout = this.logout.bind(this);
         const locale = localStorage.getItem('locale');
         this.state = {
-            'locale':  locale
+            'locale': locale
         };
         this.switchLocaleTo(locale);
+
+        this.security = new Security();
     }
 
     switchLocale() {
@@ -35,22 +37,30 @@ export default class MainMenu extends React.Component{
             .init({lng: locale});
     }
 
+    logout() {
+        this.security.unauthenticate();
+    }
+
     render() {
-        return (<React.Fragment>
-            <header id="header">
-              <div className="inner">
-              <Link to="/" className="logo">Handwerker</Link>
-                <nav id="nav">
-                  <Link to="/">Home</Link>
-                  <Link to="/login">Login</Link>
-                  <a href="#" onClick={this.switchLocale}>Switch to {
-                      this.toggleLocale(this.state.locale)
-                  }</a>
-                </nav>
-              </div>
-            </header>
-            <a href="#menu" className="navPanelToggle"><span className="fa fa-bars" /></a>
-        </React.Fragment>
+        return (
+            <React.Fragment>
+                <header id="header">
+                    <div className="inner">
+                        <Link to="/" className="logo">Handwerker</Link>
+                        <nav id="nav">
+                            <Link to="/">Home</Link>
+                            {this.security.isAuthenticated()
+                                ? <Link to="/login" onClick={this.logout}>Logout</Link>
+                                : <Link to="/login">Login</Link>
+                            }
+                            <a href="#" onClick={this.switchLocale}>Switch to {
+                                this.toggleLocale(this.state.locale)
+                            }</a>
+                        </nav>
+                    </div>
+                </header>
+                <a href="#menu" className="navPanelToggle"><span className="fa fa-bars"/></a>
+            </React.Fragment>
         );
     }
 }
