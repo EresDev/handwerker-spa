bf1d8cd97341f853aea3d35471f1d46cdc803f8d
EresDev <hi@eresdev.com>
2020-07-08 02:59:25 +0500

    Added security to account component. A guest cannot access it anymore

--- src/Account/index.js
+++ src/Account/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { withTranslation } from 'react-i18next';
+import Cookies from 'js-cookie';
+import { Redirect } from 'react-router-dom';
 
 class Account extends React.Component {
     render() {
-        return (
-            <section id="one">
-                <div className="inner">
-                    <header>
-                        <h2>{this.props.t("common:account.heading")}</h2>
-                    </header>
-                    <p>{this.props.t("common:account.comingSoon")}</p>
-                </div>
-            </section>
-        );
+        if (Cookies.get('Authorization')) {
+            return (
+                <section id="one">
+                    <div className="inner">
+                        <header>
+                            <h2>{this.props.t("common:account.heading")}</h2>
+                        </header>
+                        <p>{this.props.t("common:account.comingSoon")}</p>
+                    </div>
+                </section>
+            );
+        } else {
+            return <Redirect to='/login' />
+        }
+
     }
 };
 
