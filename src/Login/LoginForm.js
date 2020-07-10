d78009b31cdee5f1c485c9f867e0feb24e5b17d4
EresDev <hi@eresdev.com>
2020-07-11 03:13:53 +0500

    Login errors added to form instead of showing popups.

--- src/Login/LoginForm.js
+++ src/Login/LoginForm.js
@@ -1,91 +1,101 @@
 import React from 'react';
 import { withTranslation } from 'react-i18next';
 import { withRouter } from 'react-router-dom';
-import {backendUrl} from '../globals';
+import { backendUrl } from '../globals';
 
 class LoginForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            'email': '',
-            'password': '',
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+		constructor(props) {
+				super(props);
+				this.state = {
+						email: '',
+						password: '',
+						errors: []
+				};
+				this.handleChange = this.handleChange.bind(this);
+				this.handleSubmit = this.handleSubmit.bind(this);
+		}
 
-    handleChange(event) {
-        this.setState({[event.target.id]: event.target.value});
-    }
+		handleChange(event) {
+				this.setState({ [event.target.id]: event.target.value });
+		}
 
-    async handleSubmit(event) {
-        event.preventDefault();
+		async handleSubmit(event) {
+				event.preventDefault();
 
-        const formData = new FormData();
-        formData.append('email', this.state.email);
-        formData.append('password', this.state.password);
+				const formData = new FormData();
+				formData.append('email', this.state.email);
+				formData.append('password', this.state.password);
 
-        try {
-            const res = await fetch(backendUrl + '/login_check', {
-                method: 'POST',
-                body: formData,
-                credentials: 'include'
-            });
+				try {
+						const res = await fetch(backendUrl + '/login_check', {
+								method: 'POST',
+								body: formData,
+								credentials: 'include',
+						});
 
-            if (res.status == 204) {
-                this.props.history.push('/account');
-            } else {
-                alert(this.props.t('common:login.errorInvalidCredentials'));
-            }
+						if (res.status == 204) {
+								this.props.history.push('/account');
+						} else {
+							this.setState({
+									'errors': [this.props.t('common:login.errorInvalidCredentials')]
+							});
+						}
 
-        } catch (e) {
-            alert(this.props.t('common:login.errorNetwork'));
-        }
-    }
+				} catch (e) {
+						this.setState({
+								'errors': [this.props.t('common:login.errorNetwork')]
+						});
+				}
+		}
 
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <div id="login_form">
-                    <div className="field">
-                        <label htmlFor="email"
-                               className="required">
-                            {this.props.t('common:login.email')}
-                        </label>
-                        <input type="email"
-                               id="email"
-                               required="required"
-                               value={this.state.email}
-                               onChange={this.handleChange}
-                        />
-                    </div>
-                    <div className="field">
-                        <label htmlFor="login_form_password"
-                               className="required">
-                            {this.props.t('common:login.password')}
-                        </label>
-                        <input type="password"
-                               id="password"
-                               value={this.state.password}
-                               onChange={this.handleChange}
-                               required="required"
-                               minLength={6}
-                               maxLength={4096}
-                        />
-                    </div>
-                    <div>
-                        <ul className="actions">
-                            <li>
-                                <button type="submit" className="alt">
-                                    {this.props.t('common:login.login')}
-                                </button>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </form>
-        );
-    }
+		render() {
+				return (
+						<form onSubmit={this.handleSubmit} id="login">
+								<ul className="formErrors">
+										{this.state.errors.map((value, index) => {
+												return <li>{value}</li>
+										})}
+								</ul>
+								<div id="login_form">
+										<div className="field">
+												<label htmlFor="email"
+															 className="required">
+														{this.props.t('common:login.email')}
+												</label>
+												<input type="email"
+															 id="email"
+															 required="required"
+															 value={this.state.email}
+															 onChange={this.handleChange}
+												/>
+										</div>
+										<div className="field">
+												<label htmlFor="login_form_password"
+															 className="required">
+														{this.props.t('common:login.password')}
+												</label>
+												<input type="password"
+															 id="password"
+															 value={this.state.password}
+															 onChange={this.handleChange}
+															 required="required"
+															 minLength={6}
+															 maxLength={4096}
+												/>
+										</div>
+										<div>
+												<ul className="actions">
+														<li>
+																<button type="submit" className="alt">
+																		{this.props.t('common:login.login')}
+																</button>
+														</li>
+												</ul>
+										</div>
+								</div>
+						</form>
+				);
+		}
 };
 
 export default withTranslation()(withRouter(LoginForm));
--- src/Shared/MainMenu.js
+++ src/Shared/MainMenu.js
@@ -1,68 +1,68 @@
 import React from 'react';
-import {Link, withRouter} from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import i18n from 'i18next';
 import { withTranslation, initReactI18next } from 'react-i18next';
 import Security from '../Util/Security';
 
 export class MainMenu extends React.Component {
-    constructor(props) {
-        super(props);
-        this.switchLocale = this.switchLocale.bind(this);
-        this.logout = this.logout.bind(this);
-        const locale = localStorage.getItem('locale');
-        this.state = {
-            'locale': locale
-        };
-        this.switchLocaleTo(locale);
+  constructor(props) {
+    super(props);
+    this.switchLocale = this.switchLocale.bind(this);
+    this.logout = this.logout.bind(this);
+    const locale = localStorage.getItem('locale');
+    this.state = {
+        locale: locale,
+      };
+    this.switchLocaleTo(locale);
 
-        this.security = new Security();
-    }
+    this.security = new Security();
+  }
 
-    switchLocale() {
-        const currentLocale = this.state.locale;
-        const toggledLocale = this.toggleLocale(currentLocale);
-        this.setState({locale: toggledLocale});
-        this.switchLocaleTo(toggledLocale);
+  switchLocale() {
+    const currentLocale = this.state.locale;
+    const toggledLocale = this.toggleLocale(currentLocale);
+    this.setState({ locale: toggledLocale });
+    this.switchLocaleTo(toggledLocale);
 
-        localStorage.setItem('locale', toggledLocale);
-    }
+    localStorage.setItem('locale', toggledLocale);
+  }
 
-    toggleLocale(locale) {
-        return locale === 'en' || locale === null ? 'de' : 'en';
-    }
+  toggleLocale(locale) {
+    return locale === 'en' || locale === null ? 'de' : 'en';
+  }
 
-    switchLocaleTo(locale) {
-        i18n
-            .use(initReactI18next) // passes i18n down to react-i18next
-            .init({lng: locale});
-    }
+  switchLocaleTo(locale) {
+    i18n
+        .use(initReactI18next) // passes i18n down to react-i18next
+        .init({ lng: locale });
+  }
 
-    logout() {
-        this.security.unauthenticate();
-    }
+  logout() {
+    this.security.unauthenticate();
+  }
 
-    render() {
-        return (
-            <React.Fragment>
+  render() {
+    return (
+        <React.Fragment>
                 <header id="header">
                     <div className="inner">
                         <Link to="/" className="logo">Handwerker</Link>
                         <nav id="nav">
-                            <Link to="/">{this.props.t("common:menu.home")}</Link>
+                            <Link to="/">{this.props.t('common:menu.home')}</Link>
                             {this.security.isAuthenticated()
-                                ? <Link to="/login" onClick={this.logout}>{this.props.t("common:menu.logout")}</Link>
-                                : <Link to="/login">{this.props.t("common:menu.login")}</Link>
-                            }
+                            ? <Link to="/login" onClick={this.logout}>{this.props.t('common:menu.logout')}</Link>
+                            : <Link to="/login">{this.props.t('common:menu.login')}</Link>
+                        }
                             <a href="#" onClick={this.switchLocale} title={this.props.t('common:menu.switchTo') + ' ' + this.toggleLocale(this.state.locale).toUpperCase() }>
-                                <img src={this.state.locale == 'en' ? "/images/de.png" : "/images/en.png"} className="locale"/>
+                                <img src={this.state.locale == 'en' ? '/images/de.png' : '/images/en.png'} className="locale"/>
                             </a>
                         </nav>
                     </div>
                 </header>
                 <a href="#menu" className="navPanelToggle"><span className="fa fa-bars"/></a>
             </React.Fragment>
-        );
-    }
+    );
+  }
 }
 
 export default withTranslation()(MainMenu);
