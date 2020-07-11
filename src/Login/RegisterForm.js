a918a62b659a36f6db7e5a7ff429c9a05f12f954
EresDev <hi@eresdev.com>
2020-07-12 03:25:22 +0500

    RegisterForm confirm password validation changed to be performed in react way without accessing the DOM

--- src/Login/RegisterForm.js
+++ src/Login/RegisterForm.js
@@ -3,83 +3,82 @@ import { withTranslation } from 'react-i18next';
 import { backendUrl } from '../globals';
 
 class RegisterForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+  constructor(props) {
+    super(props);
+    this.state = {
+        email: '',
+        password: '',
+        confirm_password: '',
+        errors: [],
+        confirmPasswordError: '',
+      };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const field = event.target;
+
+    this.setState(
+        { [field.id]: field.value },
+        () => {
+            if (field.id == 'confirm_password') {
+              this.validatePassword(field);
+            }
+          }
+    );
+  }
+
+  validatePassword(field) {
+    if (this.state.password !== this.state.confirm_password) {
+      field.setCustomValidity('Password and confirm password are not same.');
+    } else {
+      field.setCustomValidity('');
+    }
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append('email', this.state.email);
+    formData.append('password', this.state.password);
+
+    try {
+      const res = await fetch(backendUrl + '/user', {
+          method: 'POST',
+          body: formData,
+          credentials: 'include',
+        });
+      if (res.status == 201) {
+        alert(this.props.t('common:register.successMessage'));
+        this.setState({
             email: '',
             password: '',
             confirm_password: '',
-            errors: []
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(event) {
-        const form = event
-            .target
-            .parentNode
-            .parentNode;
-
-        this.setState(
-            {[event.target.id]: event.target.value},
-            () => { this.validatePassword(form) }
-        );
-    }
-
-    validatePassword(form) {
-        const confirmPassword = form.querySelector('#confirm_password');
-
-        if (this.state.password !== this.state.confirm_password) {
-            confirmPassword.setCustomValidity('Password and confirm password are not same.');
-        } else {
-            confirmPassword.setCustomValidity('');
-        }
-    }
-
-    async handleSubmit(event) {
-        console.log(this.state);
-        event.preventDefault();
-
-        const formData = new FormData();
-        formData.append('email', this.state.email);
-        formData.append('password', this.state.password);
-
-        try {
-            const res = await fetch(backendUrl + '/user', {
-                method: 'POST',
-                body: formData,
-                credentials: 'include'
-            });
-            if (res.status == 201) {
-                alert(this.props.t('common:register.successMessage'));
-                this.setState({
-                    email: '',
-                    password: '',
-                    confirm_password: ''
-                });
-            } else {
-                const resBody = await res.json();
-                if (resBody.status == 'fail') {
-                    this.setState({
-                        errors: [JSON.stringify(resBody.data)],
-                    });
-                }
-            }
-        } catch (e) {
-            this.setState({
-                errors: [this.props.t('common:register.errorNetwork')],
+          });
+      } else {
+        const resBody = await res.json();
+        if (resBody.status == 'fail') {
+          this.setState({
+              errors: [JSON.stringify(resBody.data)],
             });
         }
+      }
+    } catch (e) {
+      this.setState({
+          errors: [this.props.t('common:register.errorNetwork')],
+        });
     }
+  }
 
-    render() {
-        return (
-                <form onSubmit={this.handleSubmit}>
+  render() {
+    return (
+            <form onSubmit={this.handleSubmit}>
                     <ul className="formErrors">
                         {this.state.errors.map((value, index) => {
-                            return <li>{value}</li>;
-                        })}
+                        return <li>{value}</li>;
+                      })}
                     </ul>
                     <div id="register_form">
                         <div className="field">
@@ -87,31 +86,31 @@ class RegisterForm extends React.Component {
                                 {this.props.t('common:register.email')}
                             </label>
                             <input type="email" id="email" required="required"
-                                   value={this.state.email}
-                                   onChange={this.handleChange}
-                            />
+                               value={this.state.email}
+                               onChange={this.handleChange}
+                        />
                         </div>
                         <div className="field">
                             <label htmlFor="password"
-                                   className="required">
+                               className="required">
                                 {this.props.t('common:register.password')}
                             </label><input
-                            type='password' id="password"
-                            required="required" minLength={6} maxLength={4096}
-                            value={this.state.password}
-                            onChange={this.handleChange}
-                        />
+                        type='password' id="password"
+                        required="required" minLength={6} maxLength={4096}
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                    />
                         </div>
                         <div className="field"><label htmlFor="confirm_password"
-                                                      className="required">
+                                                  className="required">
                             {this.props.t('common:register.confirmPassword')}
                         </label><input
-                            type="password" id="confirm_password"
-                            required="required" minLength={6}
-                            maxLength={4096}
-                            value={this.state.confirm_password}
-                            onChange={this.handleChange}
-                        /></div>
+                        type="password" id="confirm_password"
+                        required="required" minLength={6}
+                        maxLength={4096}
+                        value={this.state.confirm_password}
+                        onChange={this.handleChange}
+                    /></div>
                         <div>
                             <ul className="actions">
                                 <li>
@@ -123,8 +122,8 @@ class RegisterForm extends React.Component {
                         </div>
                     </div>
                 </form>
-        );
-    }
+    );
+  }
 }
 
 export default withTranslation()(RegisterForm);
