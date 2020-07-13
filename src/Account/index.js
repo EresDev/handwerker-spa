c26c0e994d6c891914ccba4b514bf00f146bf9f9
EresDev <hi@eresdev.com>
2020-07-13 22:32:21 +0500

    First set of login tests passing

--- src/Account/index.js
+++ src/Account/index.js
@@ -2,27 +2,27 @@ import React from 'react';
 import { withTranslation } from 'react-i18next';
 import Cookies from 'js-cookie';
 import { Redirect } from 'react-router-dom';
-import Security from "../Util/Security";
+import Security from '../Util/Security';
 
 class Account extends React.Component {
-    render() {
-        const security = new Security();
-        if (security.isAuthenticated()) {
-            return (
-                <section id="one">
+  render() {
+    const security = new Security();
+    if (security.isAuthenticated()) {
+      return (
+          <section id="one">
                     <div className="inner">
                         <header>
-                            <h2>{this.props.t("common:account.heading")}</h2>
+                            <h2>{this.props.t('common:account.heading')}</h2>
                         </header>
-                        <p>{this.props.t("common:account.comingSoon")}</p>
+                        <p>{this.props.t('common:account.comingSoon')}</p>
                     </div>
                 </section>
-            );
-        } else {
-            return <Redirect to='/login' />
-        }
-
+      );
+    } else {
+      return <Redirect to='/login' />;
     }
+
+  }
 };
 
 export default withTranslation()(Account);
---
+++ src/Login/LoginForm.test.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import { LoginForm } from './LoginForm';
+import { configure, shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+const email = 'test@eresdev.com';
+const password = '9g8yr2r83';
+
+test('Form handleSubmit is called on submit - shallow', () => {
+
+  const translator = (key) => key;
+  const wrapper = shallow(
+    <LoginForm t={translator}/>
+  );
+  wrapper.instance().handleSubmit = jest.fn();
+  wrapper.update();
+
+  const email = wrapper.find('#email');
+  expect(email.length).toEqual(1);
+  email.simulate('change', { target: { value: 'test@eresdev.com' } });
+
+  const password = wrapper.find('#password');
+  expect(password.length).toEqual(1);
+  password.simulate('change', { target: { value: '9g8yr2r83' } });
+
+  wrapper.find('form').simulate('submit');
+
+  expect(wrapper.instance().handleSubmit).toHaveBeenCalled();
+});
+
+test('redirectToAccount called on successful submit', async () => {
+
+  const t = (key) => key;
+  const wrapper = mount(
+    <LoginForm t={t} />
+  );
+
+  global.fetch = jest.fn().mockImplementation(() => {
+    return {
+      status: 204,
+      body: '',
+    };
+  });
+
+  wrapper.instance().redirectToAccount = jest.fn();
+  wrapper.update();
+
+  await submitFormWith(wrapper, email, password);
+
+  assertInputFieldsUpdate(wrapper, email, password);
+
+  expect(wrapper.instance().redirectToAccount).toHaveBeenCalled();
+
+  global.fetch.mockClear();
+  delete global.fetch;
+});
+
+test('Invalid credentials error on submit', async () => {
+
+  const t = (key) => key;
+  const wrapper = mount(
+    <LoginForm t={t} />
+  );
+
+  global.fetch = jest.fn().mockImplementation(() => {
+    return {
+      status: 401,
+      body: '',
+    };
+  });
+
+  await submitFormWith(wrapper, email, password);
+
+  assertInputFieldsUpdate(wrapper, email, password);
+
+  wrapper.update();
+  expect(wrapper.find('.formErrors li').length).toEqual(1);
+  expect(wrapper.find('.formErrors li').text()).toEqual('common:login.errorInvalidCredentials');
+
+  global.fetch.mockClear();
+  delete global.fetch;
+});
+
+async function submitFormWith(wrapper, email, password) {
+  wrapper.find('#email').getDOMNode().value = email;
+  wrapper.find('#email').simulate('change');
+  wrapper.find('#password').getDOMNode().value = password;
+  wrapper.find('#password').simulate('change');
+
+  await wrapper.find('form').simulate('submit');
+}
+
+function assertInputFieldsUpdate(wrapper, email, password) {
+  expect(wrapper.find('form').length).toEqual(1);
+  expect(wrapper.find('#email').length).toEqual(1);
+  expect(wrapper.find('#email').getDOMNode().value).toEqual(email);
+  expect(wrapper.find('#password').getDOMNode().value).toEqual(password);
+}
